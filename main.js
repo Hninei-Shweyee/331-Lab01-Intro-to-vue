fdbb8c3bcd72633494d41a72535a9c232195f69b
Hnin Ei Shwe Yee <eishweyee3110@gmail.com>
2025-06-27 10:41:07 +0700

    8 to 8.5

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
-const {createApp,ref}= Vue
+// const {createApp,ref}= Vue
+const {createApp,ref,computed}= Vue
     createApp({
         setup(){
             const product=ref('Boots')
             // const description=ref('These are the best boots ever!')
-            const image=ref('./assets/images/socks_green.jpg')
-            const inStock=ref(true)
+            // const image=ref('./assets/images/socks_green.jpg')
+            // const inStock=ref(true)
             const inventory=ref(100)
             const details=ref([
                 '50% cotton',
                 '30% wool',
                 '20% polyester'
             ])
+            const saleMessage=computed(()=>{
+                return `${brand.value} ${product.value} is on sale`;
+            });
+            const brand=ref('Nike')
             const variants=ref([
                 {
-                    id: 2234,color: 'green',image: './assets/images/socks_green.jpg'
+                    id: 2234,color: 'green',image: './assets/images/socks_green.jpg',quantity:50
                 },
                 {
-                    id: 2235,color: 'blue',image: './assets/images/socks_blue.jpg'
+                    id: 2235,color: 'blue',image: './assets/images/socks_blue.jpg',quantity:0
                 }
             ])
+            const selectedVariant=ref(0)
+            function updateVariant(index){
+                selectedVariant.value=index;
+            }
+            const image=computed(()=>{
+                return variants.value[selectedVariant.value].image
+            })
+            const inStock=computed(()=>{
+                return variants.value[selectedVariant.value].quantity
+            })
             const cart=ref(0)
             function addToCart(){
                 cart.value+=1
             }
+            const title=computed(()=>{
+                return brand.value+' '+product.value
+            })
             function updateImage(variantImage){
                 image.value=variantImage
             }
@@ -32,7 +50,7 @@ const {createApp,ref}= Vue
             const sales=ref(true)
             const cmuLink=ref('https://www.camt.cmu.ac.th')
             return{
-                product,
+                // product,
                 image,
                 cmuLink,
                 inStock,
@@ -44,7 +62,11 @@ const {createApp,ref}= Vue
                 cart,
                 addToCart,
                 updateImage,
-                toggleStock
+                toggleStock,
+                title,
+                updateVariant,
+                saleMessage,
+                brand
             }
 
         }
